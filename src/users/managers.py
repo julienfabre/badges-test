a36da3bb5a89c5491d43398e1aca1e063ce1b696
Julien Fabre <contact@jfabre.com>
2018-10-01 16:13:31 +0200

    Custom UserManager to award Pioneer badges.

--- src/users/managers.py
+++ src/users/managers.py
@@ -2,18 +2,22 @@ from datetime import timedelta
 
 from django.utils import timezone
 from django.contrib.auth.models import UserManager
-
-import badges
+from django.apps import apps
 
 
 class SketchfabUserManager(UserManager):
     def get(self, *args, **kwargs):
         """
-        Overriding get method to award Pioneer badge at login.
+        Overriding get method from QuerySet to award Pioneer badge at login.
         """
         user = super(UserManager, self).get(*args, **kwargs)
-        if (user.date_joined < timezone.now() - timedelta(days=365)) and user.pioneer.count() == 0:
-            pioneer = badges.models.Pioneer()
-            pioneer.user = user
-            pioneer.save()
+
+        # Lazy loading Pioneer model to avoid circular imports.
+        pioneer_model = apps.get_model(app_label="badges", model_name="Pioneer")
+
+        if (user.date_joined < timezone.now() - timedelta(days=365)) and \
+                not pioneer_model.objects.filter(user=user).exists():
+            # Awarding Pioneer badge.
+            pioneer_model.objects.create(user=user)
+
         return user
