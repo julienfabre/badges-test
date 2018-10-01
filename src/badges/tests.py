de00b2e9d5dc5701225cface4df36272bac73a2a
Julien Fabre <contact@jfabre.com>
2018-10-01 16:11:53 +0200

    Reworked badges tests.

--- src/badges/tests.py
+++ src/badges/tests.py
@@ -1,9 +1,10 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
+
+from django.utils import timezone
 from django.test import TestCase
-from django.contrib.auth.models import User
 
+from users.models import User
 from models3d.models import Model
-
 from .models import Star, Collector, Pioneer
 
 
@@ -13,7 +14,7 @@ class BadgeTestCase(TestCase):
         self.user = User.objects.create_user(
             username='test',
             password='password',
-            date_joined=datetime.now() - timedelta(days=365),
+            date_joined=timezone.now() - timedelta(days=365),
         )
 
     def test_star(self):
@@ -34,4 +35,5 @@ class BadgeTestCase(TestCase):
         self.assertTrue(Collector.objects.filter(user=self.user))
 
     def test_pioneer(self):
+        self.client.login(username=self.user.username, password='password')
         self.assertTrue(Pioneer.objects.filter(user=self.user))
